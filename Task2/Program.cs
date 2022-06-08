c81544ef4522a9c30e84f2c7f9301a993da3ea1b
Felix Olenko <0feliks0@gmail.com>
2022-06-08 21:58:25 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите первое число: ");
+string paramString1 = Console.ReadLine();
+int paramInt1 = int.Parse(paramString1);
+
+Console.Write("Введите второе число: ");
+string paramString2 = Console.ReadLine();
+int paramInt2 = int.Parse(paramString2);
+
+Console.Write("Введите третье число: ");
+string paramString3 = Console.ReadLine();
+int paramInt3 = int.Parse(paramString3);
+
+int max = paramInt1;
+
+if (max < paramInt2)
+{
+    max = paramInt2;    
+}
+
+if (max < paramInt3)
+{
+    max = paramInt3;
+
+}
+
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(max);
