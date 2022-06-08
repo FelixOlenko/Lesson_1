3415b5b6c48dbbfb23d9693c2c411ca4aadb4484
Felix Olenko <0feliks0@gmail.com>
2022-06-08 21:59:34 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse(paramString);
+
+
+if (paramInt % 2 == 0)
+{
+    Console.Write("Число чётное: ");
+    Console.WriteLine(paramInt);    
+}
+
+else 
+{
+    Console.Write("Число нечётное: ");
+    Console.WriteLine(paramInt);    
+}
