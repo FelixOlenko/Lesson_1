cc72b7628d7942010f2472a6516c9ff03c7628f2
Felix Olenko <0feliks0@gmail.com>
2022-06-08 21:57:03 +0300

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число: ");
+string paramString1 = Console.ReadLine();
+int paramInt1 = int.Parse(paramString1);
+Console.Write("Введите второе число: ");
+string paramString2 = Console.ReadLine();
+int paramInt2 = int.Parse(paramString2);
+
+
+if(paramInt1 < paramInt2)
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(paramInt2);
+    Console.Write("Меньшее число = ");
+    Console.WriteLine(paramInt1);
+
+}
+else
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(paramInt1);
+    Console.Write("Меньшее число = ");
+    Console.WriteLine(paramInt2);
+}
